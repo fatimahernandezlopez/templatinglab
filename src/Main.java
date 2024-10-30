74f61d45e4e5eab0bf5535c254a5413f49a6824c
fatimahernandezlopez <156130862+fatimahernandezlopez@users.noreply.github.com>
2024-10-30 12:58:14 -0500

    Made template

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,78 @@
+import java.util.ArrayList;
+
+interface Ingredient{
+    String getName();
+    double getQuantity();
+}
+
+class SolidIngredient implements Ingredient{
+    private String _name;
+    private double _quantity;
+
+    public SolidIngredient(String name, double quantity){
+        _name = name;
+        _quantity = quantity;
+    }
+    public String getName(){
+        return _name;
+    }
+    public double getQuantity(){
+        return _quantity;
+    }
+}
+
+
+class LiquidIngredient implements Ingredient{
+    private String _name;
+    private double _quantity;
+
+    public LiquidIngredient(String name, double quantity){
+        _name = name;
+        _quantity = quantity;
+    }
+    public String getName(){
+        return _name;
+    }
+    public double getQuantity(){
+        return _quantity;
+    }
+}
+
+
+class Recipe<T extends Ingredient> {
+    private String _name;
+    private ArrayList<T> _ingredients;
+    public Recipe(String name, int size){
+        _ingredients = new ArrayList<>(size);
+    }
+
+    public void addIngredient(T t){
+        _ingredients.add(t);
+
+    }
+
+    public void print(){
+        for(T t : _ingredients){
+            System.out.println(t);
+        }
+
+    }
+
+
+}
 
 
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("fsti");
+        Recipe<Ingredient> r = new Recipe<>("Cookies", 12);
+        SolidIngredient s = new SolidIngredient("sugar", 3);
+        r.addIngredient();
+
+
+
+
+
     }
 }
